a29a497c709447c802ac7a60c6bc43b106139289
tc252 <contato.tereza25@gmail.com>
2024-09-16 22:40:16 +0000

    atualizando código

---
+++ tiposPrimitivos.js
@@ -0,0 +1,9 @@
+let rotulo = "Natalya" //declaramos a variável
+rotulo = 12345 // reatribuimos o tipo de dado
+console.log(rotulo);
+
+
+const num1 = 10
+const num2 = 20
+const calc = num1 - num2
+console.log("O resultado é " + calc)
